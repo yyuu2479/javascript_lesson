3d2f5f9855ef09c125b9a4308c54c6701f19599e
Y. Y <yuu.yama@wainoMacBook-Air.local>
2021-08-22 22:16:15 +0900

    javascript2

--- javascript2/app.js
+++ javascript2/app.js
@@ -1,2 +1,29 @@
-let hello = "Hello, World"
+let hello = "Hello, World";
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14;
+let str1 = "JavaScriptを覚えよう ";
+let str2 = "Let Go !!!"
+
+let orange = 200;
+let apple = 120;
+
 alert(hello);
+alert(int1);
+alert(int2);
+alert(float1);
+alert(str1);
+alert(4+3);
+alert(4-3);
+alert(4*3);
+alert(12/3);
+alert("Hello" + "World");
+alert(str1 + str2);
+
+if (orange < apple){
+  alert("みかんの値段はりんごよりも安い")
+} else if(orange == apple){
+  alert("みかんとりんごは同じ値段")
+} else{
+  alert("みかんはりんごよりも高い")
+}
