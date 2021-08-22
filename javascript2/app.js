6fda783e57b3fc0b4081667927fef0d8879377e3
Y. Y <yuu.yama@wainoMacBook-Air.local>
2021-08-22 23:47:49 +0900

    javascript2

--- javascript2/app.js
+++ javascript2/app.js
@@ -1,29 +1,12 @@
-let hello = "Hello, World";
-let int1 = 1;
-let int2 = -10;
-let float1 = 3.14;
-let str1 = "JavaScriptを覚えよう ";
-let str2 = "Let Go !!!"
+num1 = 10;
+num2 = 10;
 
-let orange = 200;
-let apple = 120;
+number1 = num1+num2
+number2 = num1-num2
+number3 = num1*num2
+number4 = num1/num2
 
-alert(hello);
-alert(int1);
-alert(int2);
-alert(float1);
-alert(str1);
-alert(4+3);
-alert(4-3);
-alert(4*3);
-alert(12/3);
-alert("Hello" + "World");
-alert(str1 + str2);
-
-if (orange < apple){
-  alert("みかんの値段はりんごよりも安い")
-} else if(orange == apple){
-  alert("みかんとりんごは同じ値段")
-} else{
-  alert("みかんはりんごよりも高い")
-}
+alert(num1 + "+" + num2 + "=" + number1)
+alert(num1 + "-" + num2 + "=" + number2)
+alert(num1 + "*" + num2 + "=" + number3)
+alert(num1 + "/" + num2 + "=" + number4)
