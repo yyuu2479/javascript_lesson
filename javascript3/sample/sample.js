0c8d39fb03ab106c78a6110d927618d1d5bd3e3c
Y. Y <yuu.yama@wainoMacBook-Air.local>
2021-08-23 21:41:48 +0900

    javascript3

---
+++ javascript3/sample/sample.js
@@ -0,0 +1,19 @@
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
+
+
+
+function fot(){
+  console.log(int)
+  var int = "1"
+  console.log(int)
+}
+
+fot()
