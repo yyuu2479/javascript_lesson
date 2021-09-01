5dab086de72d6949516b2d64cb26c397656b7a3e
yyuu2479@gmail.com <yyuu2479@gmail.com>
2021-09-01 19:56:09 +0900

    javascript11

---
+++ javascript11/script.js
@@ -0,0 +1,27 @@
+const API_KEY = "e06f0ec444f90aa19fe9726eb334d26d"
+
+$(function(){
+  $('#btn').on('click', function(){
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function(data){
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function(data){
+      alert('通信に失敗しました。');
+    })
+  });
+});
